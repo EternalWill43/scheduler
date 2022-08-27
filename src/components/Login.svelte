b6f75bce8e6ca4bf72a4d22e75cd31c4c8466878
Dexaim <emilsmoney@live.com>
2022-08-27 04:51:14 -0400

    Add input validation to login

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -1,8 +1,13 @@
 <script>
     // Redirect to /register
+    let invalidInputs = false;
     function handleRegister(e) {
         location.href = "/register";
     }
+        if (uname.length < 1 || pword.length < 1) {
+            invalidInputs = true;
+            return;
+        }
 </script>
 
 <div class="flex flex-col justify-center">
@@ -26,6 +31,25 @@
             class="cursor-pointer text-center block m-2 p-2 rounded hover:bg-sky-700 bg-sky-600"
         >
             Register
+            {#if invalidInputs}
+                <div class="alert alert-error shadow-lg">
+                    <div>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="stroke-current flex-shrink-0 h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            ><path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                            /></svg
+                        >
+                        <span>Error! Not valid input.</span>
+                    </div>
+                </div>
+            {/if}
         </div>
     </div>
 </div>
