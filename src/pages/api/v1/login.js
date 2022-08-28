4f74dc121dcbf9ebc0ec38cb06976f2e096022df
Dex Aim <emilsmoney@live.com>
2022-08-28 11:56:09 -0400

    Prettier styling

--- src/pages/api/v1/login.js
+++ src/pages/api/v1/login.js
@@ -5,11 +5,28 @@ import { connectToDatabase } from "../../../../lib/db";
 export async function post({ request }) {
     let { db } = await connectToDatabase();
     const { uname, pword } = await request.json();
-    let user = await db.collection('users').findOne({ username: uname.toLowerCase() });
-    if (!user) return new Response(JSON.stringify({ error: "User not found" }), { status: 404 });
+    let user = await db
+        .collection("users")
+        .findOne({ username: uname.toLowerCase() });
+    if (!user)
+        return new Response(JSON.stringify({ error: "User not found" }), {
+            status: 404,
+        });
     let valid = bcrypt.compareSync(pword, user.password);
-    if (!valid) return new Response(JSON.stringify({ error: "Invalid password" }), { status: 401 });
+    if (!valid)
+        return new Response(JSON.stringify({ error: "Invalid password" }), {
+            status: 401,
+        });
     let headers = new Headers();
-    headers.append("Set-Cookie", `token=${jsonwebtoken.sign({ username: user.username }, import.meta.env.JWT_SECRET)}; Path=/; HttpOnly; SameSite=Strict`);
-    return new Response(JSON.stringify({ success: true }), { status: 200, headers });
+    headers.append(
+        "Set-Cookie",
+        `token=${jsonwebtoken.sign(
+            { username: user.username },
+            import.meta.env.JWT_SECRET
+        )}; Path=/; HttpOnly; SameSite=Strict`
+    );
+    return new Response(JSON.stringify({ success: true }), {
+        status: 200,
+        headers,
+    });
 }
