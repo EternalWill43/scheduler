2c1e08973b6ae04ea26592f428e0fe1a17de5147
Dex Aim <emilsmoney@live.com>
2022-08-29 10:14:10 -0400

    Styling - longer lines

--- src/pages/api/v1/addemployee.js
+++ src/pages/api/v1/addemployee.js
@@ -1,17 +1,8 @@
 import { addEmployee } from "../../../../lib/ps/users/employees";
 
 export async function post({ request }) {
-    const { firstName, lastName, department, shift, day1, day2 } =
-        await request.json();
-    console.log(firstName, lastName, department, shift, day1, day2);
-    let res = await addEmployee(
-        firstName,
-        lastName,
-        department,
-        shift,
-        day1,
-        day2
-    );
+    const { firstName, lastName } = await request.json();
+    let res = await addEmployee(firstName, lastName, department, shift, day1, day2);
     return new Response(JSON.stringify({ success: true }), {
         status: 200,
     });
