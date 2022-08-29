604a63bcabcc75cbad53bbb3e5e2ace31a69819a
Dex Aim <emilsmoney@live.com>
2022-08-29 10:15:14 -0400

    Remove unused daisyUI themes from tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-	},
-	plugins: [require("daisyui")],
-	daisyui: {
-		themes: ["halloween", "dark", "business"],
-	},
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: "class",
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["business"],
+    },
+};
