be0a32531e3a8a7ef97f586c768af330fef53388
Dex Aim <emilsmoney@live.com>
2022-08-30 11:11:48 -0400

    New style navbar

---
+++ src/components/NavLink.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let url: string;
+    export let name: string;
+    export let active: string = "text-gray-400";
+</script>
+
+<li>
+    <a
+      href="{url}"
+      class="block py-2 pr-4 pl-3 {active} bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:{active}"
+      aria-current="page">{name}</a
+    >
+</li>
