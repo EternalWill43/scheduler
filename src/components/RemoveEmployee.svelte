31f8492250b9a4f8492b012a5863113c6e2b20ce
Dex Aim <emilsmoney@live.com>
2022-09-01 06:40:43 -0400

    Fix styling on remove employee component

--- src/components/RemoveEmployee.svelte
+++ src/components/RemoveEmployee.svelte
@@ -9,17 +9,19 @@
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                "firstName": firstName,
-                "lastName": lastName,
-            }), 
-        }).then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err));
+                firstName: firstName,
+                lastName: lastName,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err));
     }
 </script>
 
-<div>
-    <input type="text" bind:value={firstName}/>
-    <input type="text" bind:value={lastName}/>
-    <button on:click={handleSubmit}>Remove Employee</button>
+<div class="w-full flex flex-col align-middle items-center">
+    <div class="font-bold text-xl m-2">Remove Employee</div>
+    <input class="w-full input input-bordered max-w-xs" type="text" bind:value={firstName} placeholder="First Name" />
+    <input class="w-full input input-bordered max-w-xs" type="text" bind:value={lastName} placeholder="Last Name" />
+    <button class="m-2 w-1/2 btn btn-primary" on:click={handleSubmit}>Remove Employee</button>
 </div>
