1c07d7c5a98ea5d0c4b851d1fcfad5ee3d70ec9e
Dex Aim <emilsmoney@live.com>
2022-09-01 06:40:15 -0400

    Fix styling in add employee component

--- src/components/AddEmployee.svelte
+++ src/components/AddEmployee.svelte
@@ -12,25 +12,42 @@
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                "firstName": firstName,
-                "lastName": lastName,
-                "department": department,
-                "shift": shift,
-                "day1": day1,
-                "day2": day2,
-            }), 
-        }).then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err));
+                firstName: firstName,
+                lastName: lastName,
+                department: department,
+                shift: shift,
+                day1: day1,
+                day2: day2,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err));
     }
 </script>
 
-<div>
-    <input type="text" bind:value={firstName}/>
-    <input type="text" bind:value={lastName}/>
-    <input type="text" bind:value={department}/>
-    <input type="text" bind:value={shift}/>
-    <input type="text" bind:value={day1}/>
-    <input type="text" bind:value={day2}/>
-    <button on:click={handleSubmit}>Submit Employee</button>
+<div class="flex flex-col m-2 align-middle justify-center items-center w-full">
+    <div class="font-bold text-xl m-2">Add Employee</div>
+    <input class="w-full input input-bordered max-w-xs" type="text" bind:value={firstName} placeholder="First Name" />
+    <input class="w-full input input-bordered max-w-xs" type="text" bind:value={lastName} placeholder="Last Name" />
+    <input
+        class="w-full input input-bordered max-w-xs"
+        type="text"
+        bind:value={department}
+        placeholder="Department Name"
+    />
+    <input class="w-full input input-bordered max-w-xs" type="text" bind:value={shift} placeholder="Shift i.e., 2200" />
+    <input
+        class="w-full input input-bordered max-w-xs"
+        type="text"
+        bind:value={day1}
+        placeholder="First day off i.e., Sunday"
+    />
+    <input
+        class="w-full input input-bordered max-w-xs"
+        type="text"
+        bind:value={day2}
+        placeholder="Second day off i.e., Monday"
+    />
+    <button class="w-1/2 m-2 btn btn-primary" on:click={handleSubmit}>Submit Employee</button>
 </div>
