6233d45dbe10e072f2916fca63d537f23fcb1723
Dexaim <emilsmoney@live.com>
2022-09-03 04:11:57 -0400

    Create API endpoint for adding departments

---
+++ src/pages/api/v1/adddepartment.js
@@ -0,0 +1,9 @@
+import { addDepartment } from "../../../../lib/ps/users/departments";
+
+export async function post({ request }) {
+    let body = await request.json();
+    let res = await addDepartment(body.department);
+    return new Response(JSON.stringify(res), {
+        status: 200,
+    });
+}
