56d5a5e4f8ad9bb2a338c35e77ef90475763626b
Dexaim <emilsmoney@live.com>
2022-09-04 03:38:29 -0400

    Remove console logs from server

--- src/pages/api/v1/getdepartments.js
+++ src/pages/api/v1/getdepartments.js
@@ -2,7 +2,6 @@ import { getDepartments } from "../../../../lib/ps/users/departments";
 
 export async function get({ request }) {
     let res = await getDepartments();
-    console.log(res);
     return new Response(JSON.stringify(res), {
         status: 200,
     });
