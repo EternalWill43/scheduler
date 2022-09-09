1f7f47b0567c6880870f9e2bd4ac3244c8567504
Dexaim <emilsmoney@live.com>
2022-09-09 03:51:50 -0400

    Fix navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,34 +5,43 @@
     onMount(() => (url = window.location.href));
 </script>
 
-<div class="navbar bg-grey-900">
+<div class="navbar bg-base-100">
     <div class="flex-1">
         <a href="/" class="btn btn-ghost normal-case text-xl">Scheduler</a>
     </div>
-    <div class="flex-none">
-        <ul class="menu menu-horizontal p-0">
-            <li tabindex="0">
-                <a href="/">
-                    Employees
-                    <svg
-                        class="fill-current"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        ><path
-                            d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"
-                        /></svg
-                    >
-                </a>
-                <ul class="p-2 bg-base-100">
-                    <li><a href="/employees">Employee List</a></li>
-                    <li><a href="/manage">Manage Employees</a></li>
-                    <li><a href="/vacation">Vacations</a></li>
-                </ul>
-            </li>
-            <li><a href="/departments">Departments</a></li>
-            <li><a href="/shifts">Shifts</a></li>
+    <div class="dropdown dropdown-end">
+        <!-- svelte-ignore a11y-label-has-associated-control -->
+        <label tabindex="0" class="btn bg-transparent border-none">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
+                />
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+            </svg>
+        </label>
+
+        <ul
+            tabindex="0"
+            class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
+        >
+            <li><a href="/vacation">Manage Vacations</a></li>
+            <li><a href="/manage">Manage Employees</a></li>
+            <li><a href="/shifts">Manage Shifts</a></li>
+            <li><a href="/departments">Manage Departments</a></li>
+            <li><a href="/employees">View Employee List</a></li>
         </ul>
     </div>
 </div>
