57c4e87ecaf23880fb112373dfa9cbe393b5198b
Dexaim <emilsmoney@live.com>
2022-09-09 04:44:21 -0400

    Add success information to API endpoint

--- src/pages/api/v1/getemployeevacation.js
+++ src/pages/api/v1/getemployeevacation.js
@@ -3,6 +3,7 @@ import { getEmployeeVacation } from "../../../../lib/ps/users/employees";
 export async function post({ request }) {
     let { firstName, lastName } = await request.json();
     let res = await getEmployeeVacation(firstName, lastName);
+    res.push({ success: true });
     return new Response(JSON.stringify(res), {
         status: 200,
     });
