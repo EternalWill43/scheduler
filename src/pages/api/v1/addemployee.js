1fc4fbd6123cd14f1af5e94273640bbf31ea6952
Dexaim <emilsmoney@live.com>
2022-09-12 05:32:29 -0400

    Make affectedRows optional in case the query fails

--- src/pages/api/v1/addemployee.js
+++ src/pages/api/v1/addemployee.js
@@ -3,7 +3,7 @@ import { addEmployee } from "../../../../lib/ps/users/employees";
 export async function post({ request }) {
     const { firstName, lastName, department, shift, day1, day2 } = await request.json();
     let res = await addEmployee(firstName, lastName, department, shift, day1, day2);
-    if (res.affectedRows > 0) {
+    if (res?.affectedRows > 0) {
         return new Response(JSON.stringify({ success: true }), {
             status: 200,
         });
--- src/pages/api/v1/setvacation.ts
+++ src/pages/api/v1/setvacation.ts
@@ -4,7 +4,7 @@ import { setVacation } from "../../../../lib/ps/users/employees";
 export async function post(_params: any, request: Request) {
     const { firstName, lastName, vacList } = await request.json();
     let res = await setVacation(firstName, lastName, vacList);
-    if (res.affectedRows > 0) {
+    if (res?.affectedRows > 0) {
         return new Response(JSON.stringify({ success: true }), {
             status: 200,
         });
