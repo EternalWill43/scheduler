2487dceefec69f9cacec01e389e135e51b07d19a
Dex Aim <emilsmoney@live.com>
2022-09-19 13:31:37 -0400

    Add spinner to while waiting for login

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -2,6 +2,7 @@
     let uname = "";
     let pword = "";
     let invalidInputs = false;
+    let loggingIn = false;
     function handleRegister(e) {
         location.href = "/register";
     }
@@ -11,6 +12,7 @@
             invalidInputs = true;
             return;
         }
+        loggingIn = true;
         fetch("/api/v1/login", {
             method: "POST",
             headers: {
@@ -28,6 +30,7 @@
                 } else {
                     alert(data.message);
                 }
+                loggingIn = false;
             })
             .catch((err) => {
                 console.log(err);
@@ -81,7 +84,28 @@
             />
             <div class="card-actions flex justify-evenly">
                 <button on:click={handleLogin} class="w-1/3 btn btn-primary"
-                    >Login</button
+                    >{#if loggingIn}<span
+                            ><svg
+                                class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                            >
+                                <circle
+                                    class="opacity-25"
+                                    cx="12"
+                                    cy="12"
+                                    r="10"
+                                    stroke="currentColor"
+                                    stroke-width="4"
+                                />
+                                <path
+                                    class="opacity-75"
+                                    fill="currentColor"
+                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                />
+                            </svg></span
+                        >{/if} Login</button
                 >
                 <button on:click={handleRegister} class="w-1/3 btn btn-primary"
                     >Register</button
